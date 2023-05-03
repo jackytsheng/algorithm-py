e087e272f1e73239ab501ca18f642fbeee97355a
jackytsheng <jackytsheng@gmail.com>
2023-05-03 23:00:11 +1000

    add quick sort algorithm

--- algorithms/sorting/sorting.py
+++ algorithms/sorting/sorting.py
@@ -63,6 +63,8 @@ def insert_sort(nums: list[int | float], print_detail: bool = False):
         while j > 0:
             if nums[j-1] > nums[j]:
                 nums[j], nums[j-1] = nums[j-1], nums[j]
+            else:
+                break
             j -= 1
     # Algorithm ends
     toc = time.perf_counter()
@@ -82,6 +84,98 @@ def merge_sort(nums: list[int | float], print_detail: False):
     Stable: Yes
     """
     pass
+
+
+def quick_sort_helper(nums: list[int | float]) -> list[int | float]:
+    if len(nums) < 1:
+        return nums
+    # choose the last one as pivot
+    pivot = nums[-1]
+    l = None
+    for i in range(len(nums)-1):
+        if nums[i] > pivot and l == None:
+            l = i
+        elif nums[i] < pivot:
+            if not l:
+                l = i+1
+            else:
+                nums[l], nums[i] = nums[i], nums[l]
+                l += 1
+
+    nums[l], nums[-1] = nums[-1], nums[l]
+    return quick_sort_helper(nums[:l]) + [nums[l]] + quick_sort_helper(nums[l+1:])
+
+
+# Quick sort in Python
+
+# function to find the partition position
+def quick_sort_partition(array, low, high):
+
+    # choose the rightmost element as pivot
+    pivot = array[high]
+
+    # pointer for greater element
+    i = low - 1
+
+    # traverse through all elements
+    # compare each element with pivot
+    for j in range(low, high):
+        if array[j] <= pivot:
+            # if element smaller than pivot is found
+            # swap it with the greater element pointed by i
+            i += 1
+
+            # swapping element at i with element at j
+            array[i], array[j] = array[j], array[i]
+
+    # swap the pivot element with the greater element specified by i
+    array[i + 1], array[high] = array[high], array[i + 1]
+
+    # return the position from where partition is done
+    return i + 1
+
+# function to perform quicksort
+
+
+def quick_sort_helper(array, low, high):
+    if low < high:
+
+        # find pivot element such that
+        # element smaller than pivot are on the left
+        # element greater than pivot are on the right
+        pi = quick_sort_partition(array, low, high)
+
+        # recursive call on the left of pivot
+        quick_sort_helper(array, low, pi - 1)
+
+        # recursive call on the right of pivot
+        quick_sort_helper(array, pi + 1, high)
+
+
+def quick_sort(nums: list[int | float], print_detail: bool = False) -> list[int | float]:
+    """
+    nums: Sort int array,
+    print_detail: enable detail to be print
+
+    Worst complexity: n^2
+    Average complexity: n*log(n)
+    Best complexity: n*log(n)
+    Method: Partitioning
+    Stable: No
+    Class: Comparison sort
+    """
+
+    tic = time.perf_counter()
+    # Algorithm begins
+    if len(nums) < 2:
+        toc = time.perf_counter()
+        print_detail and print_detail_helper(len(nums), toc-tic)
+        return nums
+    result = quick_sort_helper(nums, 0, len(nums)-1)
+    # Algorithm ends
+    toc = time.perf_counter()
+    print_detail and print_detail_helper(len(nums), toc-tic)
+    return result
 
 
 def print_detail_helper(element_length, time_diff):
